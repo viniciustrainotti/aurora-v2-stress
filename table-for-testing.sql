4fedefd8fff94bf0af2e045a30cf603ef9da31a0
Vinícius Trainotti <12179459+viniciustrainotti@users.noreply.github.com>
2023-01-15 17:26:07 -0300

    Create table-for-testing.sql

---
+++ table-for-testing.sql
@@ -0,0 +1,7 @@
+CREATE TABLE public.stress (
+
+);
+
+ALTER TABLE public.stress add COLUMN id SERIAL PRIMARY KEY;
+ALTER TABLE public.stress ADD column1 varchar NULL;
+ALTER TABLE public.stress ADD column2 varchar NULL;
