6e1dbf08f0c8cdfa6971d2088e7732fb7be60d54
Vinícius Trainotti <12179459+viniciustrainotti@users.noreply.github.com>
2023-01-15 21:51:21 -0300

    Update table-for-testing.sql

--- table-for-testing.sql
+++ table-for-testing.sql
@@ -1,7 +1,9 @@
 CREATE TABLE public.stress (
-
+  id serial PRIMARY KEY,
+  column1 VARCHAR ( 50 ) NULL,
+  column2 VARCHAR ( 50 ) NULL
 );
 
-ALTER TABLE public.stress add COLUMN id SERIAL PRIMARY KEY;
-ALTER TABLE public.stress ADD column1 varchar NULL;
-ALTER TABLE public.stress ADD column2 varchar NULL;
+-- ALTER TABLE public.stress add COLUMN id SERIAL PRIMARY KEY;
+-- ALTER TABLE public.stress ADD column1 varchar NULL;
+-- ALTER TABLE public.stress ADD column2 varchar NULL;
